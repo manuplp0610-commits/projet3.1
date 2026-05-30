9554e4e1a1e78fa6d19d7625353da840009b82de
Manwel Nova <manwel.nova@gmail.com>
2026-05-30 16:28:36 +0200

    ajout des filtres de categorie

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+//==================AFFICHER PROJETS DYNAMIQUEMENT DANS L ACCUEIL=====================
 // Récupère les projets depuis l'API
-async function recupererProjets() {
-  const reponse = await fetch("http://localhost:5678/api/works");
-  const data = await reponse.json();
+export async function recupererProjets() {
+  try {
+    const reponse = await fetch("http://localhost:5678/api/works");
 
-  return data;
+    if (!reponse.ok) {
+      throw new Error(`Erreur HTTP : ${reponse.status}`);
+    }
+
+    const data = await reponse.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+  }
 }
+const toutProjets = await recupererProjets();
 
 // Affiche les projets dans la galerie de la page d'accueil
-async function afficherProjetsIndex() {
-  const projets = await recupererProjets();
-  const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 
-  projets.forEach((projet) => {
+async function afficherProjetsIndex(filtre) {
+  gallery.innerHTML = "";
+
+  filtre.forEach((projet) => {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
 
@@ -26,6 +38,44 @@ async function afficherProjetsIndex() {
     gallery.appendChild(figure);
   });
 }
+afficherProjetsIndex(toutProjets);
 
-// Lance l'affichage des projets au chargement du script
-afficherProjetsIndex();
+// ===================FILTRES=================================
+const btnFiltres = document.querySelectorAll(".filtre");
+
+// fontion Choisir filtre
+function filtrerAffichageIndex() {
+  btnFiltres.forEach((btnFiltre) => {
+    btnFiltre.addEventListener("click", () => {
+      // bouton selectionné en vert
+      btnFiltres.forEach((btn) => btn.classList.remove("active"));
+      btnFiltre.classList.add("active");
+
+      // condition pour filtres
+      switch (btnFiltre.textContent) {
+        case "Tout":
+          afficherProjetsIndex(toutProjets);
+          break;
+        case "Objets":
+          const projetsObjets = toutProjets.filter(
+            (projet) => projet.category.name === "Objets",
+          );
+          afficherProjetsIndex(projetsObjets);
+          break;
+        case "Appartements":
+          const projetsAppartements = toutProjets.filter(
+            (projet) => projet.category.name === "Appartements",
+          );
+          afficherProjetsIndex(projetsAppartements);
+          break;
+        case "Hotels et restaurents":
+          const projetsHotels = toutProjets.filter(
+            (projet) => projet.category.name === "Hotels & restaurants",
+          );
+          afficherProjetsIndex(projetsHotels);
+          break;
+      }
+    });
+  });
+}
+filtrerAffichageIndex();
